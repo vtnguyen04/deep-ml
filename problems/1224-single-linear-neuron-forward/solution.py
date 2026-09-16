1b50b6fbacca63192598e14be203da5b4b4d9b19
vtnguyen04 <64852371+vtnguyen04@users.noreply.github.com>
2026-09-16 08:10:04 +0000

    Update: Single Linear Neuron Forward

--- problems/1224-single-linear-neuron-forward/solution.py
+++ problems/1224-single-linear-neuron-forward/solution.py
@@ -14,4 +14,7 @@ def single_neuron_forward(x):
     # TODO: build nn.Linear(3, 1), set fixed weight/bias under no_grad, return float output
     linear = nn.Linear(3, 1)
 
-    ư
+    with torch.no_grad():
+        linear.weight.copy_(torch.tensor([[0.5, -0.2, 0.3]]))
+        linear.bias.copy_(torch.tensor([0.1]))
+    return linear(x).item()
